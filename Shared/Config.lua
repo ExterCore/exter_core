39fc700f73009de971f21eb056c5d19ec6598061
sobing4413 <95325881+sobing4413@users.noreply.github.com>
2024-10-31 10:10:15 +0700

    Add files via upload

---
+++ Shared/Config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+--------- DO NOT MODIFY ---------
+
+Config.Framework = ExterShared.Functions.GetFramework()
+Config.Database =  ExterShared.Functions.GetDatabase()
+
+---------------------------------
